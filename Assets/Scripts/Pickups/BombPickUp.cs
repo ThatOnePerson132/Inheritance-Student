28e4c177095a22f18a539188128c12705e93bc08
ThatOnePerson132 <kjdj15243@gmail.com>
2023-05-17 08:06:35 -0700

    Start of bomb

--- Assets/Scripts/Pickups/BombPickUp.cs
+++ Assets/Scripts/Pickups/BombPickUp.cs
@@ -2,17 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BombPickUp : MonoBehaviour
+public class BombPickUp : PickUp
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
